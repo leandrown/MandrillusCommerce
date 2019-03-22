26e025bd193c52a63f990bf05151e201998867b5
Leandro Vieira Santos <leandrown@outlook.com>
2019-03-22 15:07:12 -0300

    Part 18 - Token Exceptions
    - signing credentials added;
    - Microsoft Identity Model added to references;
    -converting to Unix date/time format;

--- Mandrillus.Logics/Factories/TokenFactory.cs
+++ Mandrillus.Logics/Factories/TokenFactory.cs
@@ -1,11 +1,32 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 using Mandrillus.Contracts.Factories;
+using Mandrillus.Domain.Configurations.Auth;
 
 namespace Mandrillus.Logics.Factories
 {
    public class TokenFactory : ITokenFactory
    {
+      private readonly JwtIssuerOptions _issuerOptions;
+
+      public TokenFactory(JwtIssuerOptions issuerOptions)
+      {
+         _issuerOptions = issuerOptions;
+         ThrowInvalidOptions(_issuerOptions);
+      }
+
+      private void ThrowInvalidOptions(JwtIssuerOptions issuerOptions)
+      {
+         if (issuerOptions == null) throw new ArgumentNullException(nameof(issuerOptions));
+         if (issuerOptions.ValidUntil <= TimeSpan.Zero) throw new ArgumentNullException($"Token expiration must be greater than zero");
+         if (issuerOptions.SinSigningCredentials == null) throw new ArgumentNullException(nameof(issuerOptions.SinSigningCredentials));
+         if (issuerOptions.JtiGenerator == null) throw new ArgumentNullException(nameof(issuerOptions.JtiGenerator));
+      }
+
+      private static long ToUnixEpochDate(DateTime date) => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+
       public Task<string> GenerateIdentityToken(string username, ClaimsIdentity identity)
       {
          throw new System.NotImplementedException();
